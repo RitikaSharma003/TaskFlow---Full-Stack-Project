5763a786f37684e71c15317aaf3fcb9f79636b12
RitikaSharma003 <ritikasharma20032003@gmail.com>
2025-05-17 15:55:54 +0530

    seventh commit

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const taskApis=require("./controllers/task");
 const authMiddleware=require("./middleware/authMiddleware")
 app.use(express.json());
 app.use(cors({
-    origin:["http://localhost:5173","http://localhost:5174"],
+    origin:["http://localhost:5173","http://localhost:5174","https://task-flow-full-stack-project.netlify.app/"],
     credentials:true,
 }));
 app.use(cookieParser());
