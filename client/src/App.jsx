6f2204ce66871e9b1a2378598b4ac15a1165616a
RitikaSharma003 <ritikasharma20032003@gmail.com>
2025-05-20 00:55:16 +0530

    vercel commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,27 @@ import { useEffect } from 'react'
 //   }, [navigate, location.pathname]);
 
 
+const App = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
 
+  useEffect(() => {
+    const userLoggedIn = localStorage.getItem("userLoggedIn");
+
+    // Agar user logged in nahi hai aur dashboard par jaane ki koshish kar raha hai, toh login par redirect karo
+    if (!userLoggedIn && location.pathname === "/dashboard") {
+      navigate("/login");
+    }
+    // Agar user logged in hai aur login/register page par hai, to dashboard par redirect karo
+    else if (userLoggedIn && (location.pathname === "/login" || location.pathname === "/register")) {
+      navigate("/dashboard");
+    }
+     // Agar user logged in nahi hai aur current location login ya register nahi hai to login par redirect
+    else if (!userLoggedIn && location.pathname !== "/login" && location.pathname !== "/register")
+    {
+       navigate("/login");
+    }
+  }, [navigate, location.pathname]);
 
    
 
