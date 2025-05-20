be814240b09df536ea491ec7e233e19042782e9f
Ritika Sharma <98597040+RitikaSharma003@users.noreply.github.com>
2025-05-20 16:03:06 +0530

    commit 1 git

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,47 +4,47 @@ import Login from './pages/Login'
 import Register from "./pages/Register"
 import Dashboard from './pages/Dashboard'
 import { useEffect } from 'react'
-// const App = () => {
-
-//   const navigate=useNavigate();
-//   const location = useLocation();
-//   useEffect(() => {
-//     const userLoggedIn = localStorage.getItem("userLoggedIn");
-//     if (userLoggedIn && location.pathname !== "/dashboard") {
-//       navigate("/dashboard");
-//     } else if (!userLoggedIn && location.pathname !== "/login" && location.pathname !== "/register") {
-//       navigate("/login");
-//     }
-//   }, [navigate, location.pathname]);
-
-
-
 const App = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
 
+  const navigate=useNavigate();
+  const location = useLocation();
   useEffect(() => {
     const userLoggedIn = localStorage.getItem("userLoggedIn");
-
-    if (!userLoggedIn && location.pathname === "/dashboard") {
-      navigate("/login");
-    }
-    
-    else if (userLoggedIn && (location.pathname === "/login" || location.pathname === "/register")) {
+    if (userLoggedIn && location.pathname !== "/dashboard") {
       navigate("/dashboard");
-    }
-    
-    else if (userLoggedIn && location.pathname === "/dashboard") {
-      localStorage.removeItem("userLoggedIn");
+    } else if (!userLoggedIn && location.pathname !== "/login" && location.pathname !== "/register") {
       navigate("/login");
     }
+  }, [navigate, location.pathname]);
+
+
+
+// const App = () => {
+//   const navigate = useNavigate();
+//   const location = useLocation();
+
+//   useEffect(() => {
+//     const userLoggedIn = localStorage.getItem("userLoggedIn");
+
+//     if (!userLoggedIn && location.pathname === "/dashboard") {
+//       navigate("/login");
+//     }
     
-    else if (!userLoggedIn && location.pathname !== "/login" && location.pathname !== "/register" && location.pathname !== "/dashboard") {
-      navigate("/login");
-    }
+//     else if (userLoggedIn && (location.pathname === "/login" || location.pathname === "/register")) {
+//       navigate("/dashboard");
+//     }
+    
+//     else if (userLoggedIn && location.pathname === "/dashboard") {
+//       localStorage.removeItem("userLoggedIn");
+//       navigate("/login");
+//     }
+    
+//     else if (!userLoggedIn && location.pathname !== "/login" && location.pathname !== "/register" && location.pathname !== "/dashboard") {
+//       navigate("/login");
+//     }
    
 
-  }, [navigate, location.pathname]);
+//   }, [navigate, location.pathname]);
   
    return (
 
